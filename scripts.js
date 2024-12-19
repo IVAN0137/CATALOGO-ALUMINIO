41872f83cf4942f8137b0d98394aed92b6cff6f9
IVAN0137 <ivanmartinezgudino55@gmail.com>
2024-12-18 22:50:12 -0600

    agregure mas cosas

--- scripts.js
+++ scripts.js
@@ -1,37 +1,31 @@
+// Buscar producto
+function buscarProducto() {
+  const input = document.getElementById('buscador');
+  const filtro = input.value.toLowerCase();
+  const productos = document.querySelectorAll('.producto');
+  
+
+  productos.forEach(producto => {
+      const nombre = producto.getAttribute('data-nombre').toLowerCase();
+      if (nombre.includes(filtro)) {
+          producto.style.display = '';
+      } else {
+          producto.style.display = 'none';
+      }
+  });
+}
+
+// Ver detalles del producto
 function verDetalles(imagen) {
-  // Muestra el modal con la imagen seleccionada
-  var modal = document.getElementById("modal");
-  var modalImg = document.getElementById("modalImg");
-  modal.style.display = "block";
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modalImg');
+
+  modal.style.display = 'block';
   modalImg.src = imagen;
 }
 
+// Cerrar el modal
 function cerrarModal() {
-  // Cierra el modal
-  var modal = document.getElementById("modal");
-  modal.style.display = "none";
-}
-
-function buscarProducto() {
-  var input, filter, catalogo, secciones, productos, nombre, i, j;
-  input = document.getElementById('buscador');
-  filter = input.value.toUpperCase();
-  catalogo = document.getElementById('catalogo');
-  secciones = catalogo.getElementsByClassName('seccion');
-
-  for (i = 0; i < secciones.length; i++) {
-      productos = secciones[i].getElementsByClassName('producto');
-      var seccionVisible = false;
-      for (j = 0; j < productos.length; j++) {
-          nombre = productos[j].getAttribute('data-nombre');
-          if (nombre.toUpperCase().indexOf(filter) > -1) {
-              productos[j].style.display = "";
-              seccionVisible = true;
-          } else {
-              productos[j].style.display = "none";
-          }
-      }
-      // Si al menos un producto de la sección coincide, mostramos la sección
-      secciones[i].style.display = seccionVisible ? "block" : "none";
-  }
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
 }
